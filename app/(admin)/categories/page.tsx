25b82ef2472bc727773f2e60c21fa00b603ec126
Alhiefikri <alhiepedia@gmail.com>
2025-12-15 14:27:21 +0800

    fix categories dynamic

--- app/(admin)/categories/page.tsx
+++ app/(admin)/categories/page.tsx
@@ -1,6 +1,9 @@
 import CategoriesClient from "./client/categories-client";
 import { getCategories } from "@/app/actions/categories";
 
+export const dynamic = "force-dynamic";
+
+
 export default async function CategoriesPage() {
   const data = await getCategories();
   return (
