3ea219ea8240bd2976a6871bfc4b3d627461db20
Alhiefikri <alhiepedia@gmail.com>
2025-12-15 14:32:15 +0800

    change auth client

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,4 +1,4 @@
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
-  baseURL: "nextblog-black-seven.vercel.app",
+  baseURL: "https://nextblog-black-seven.vercel.app",
 });
